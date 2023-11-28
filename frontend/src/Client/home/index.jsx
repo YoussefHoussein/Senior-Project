3eafcdd7420d967750a9bd50057d9c376111b02d
unknown <youssefhoussein701@gmail.com>
2023-11-28 14:39:38 +0200

    frontend <> add the map to the home of the client

--- frontend/src/Client/home/index.jsx
+++ frontend/src/Client/home/index.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../Components/navbar'
 import './style.css'
 import Sidebar from '../../Components/sidebar'
+import Map from '../../Components/map'
 const ClientHome= () => {
+  const [longitude, setLongitude] = useState(5)
+  const [latitude, setLatitude] = useState(5)
   return (
     <div className='client-home relative full'>
       <Navbar username={"Youssef"} email={"test@gmail.com"} notifications={["test","test1","test2"]}/>
       <Sidebar items={["Home","Suggestions","Slots","Statistics","Support"]}/>
-      <div className="client-home-container absolute body">Home</div>
+      <div className="client-home-container absolute body">
+        <Map longitude={longitude} latitude={latitude}/>
+      </div>
     </div>
   )
 }
