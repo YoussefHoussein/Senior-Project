d48390ecbdbf6163b698b5572c19739f991a5f56
unknown <youssefhoussein701@gmail.com>
2023-11-29 09:57:36 +0200

    frontend <> add the slotcard component

--- frontend/src/Client/slots/index.jsx
+++ frontend/src/Client/slots/index.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './style.css'
 import Navbar from '../../Components/navbar'
 import Sidebar from '../../Components/sidebar'
+import SlotCard from '../../Components/slotCard'
 const Slots = () => {
   return (
     <div className='slots relative full'>
       <Navbar username={"Youssef"} />
       <Sidebar items={["Home","Suggestions","Slots","Statistics","Support"]} selected={"Slots"}/>
-      <div className="slots-body absolute body">Slots</div>
+      <div className="slots-body absolute body">
+        <SlotCard />
+      </div>
     </div>
   )
 }
---
+++ frontend/src/Components/slotCard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+const SlotCard = () => {
+  return (
+    <div className='slotcard-container'>
+
+    </div>
+  )
+}
+
+export default SlotCard
