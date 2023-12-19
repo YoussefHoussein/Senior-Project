6e35ef94eaa32c6c782b3778f62f641bf0371b76
unknown <youssefhoussein701@gmail.com>
2023-12-19 14:20:34 +0200

    backend <> delete the passport code

--- backend/index.js
+++ backend/index.js
@@ -11,21 +11,13 @@ const authRoute = require("./routes/auth")
 app.use(bodyParser.json({limit: '10mb'}))
 
 app.use(express.json)
-app.use(
-    cookieSession({
-        name:"session",
-        keys:["cyberwolve"],
-        maxAge: 24*60*60*100,
-    })
-)
-app.use(passport.initialize())
-app.use(passport.session())
+
 app.use(cors({
     origin:"http://localhost:3000",
     methods:"GET,POST,PUT,DELETE",
     credentials:true,
 }))
-app.use("/auth",authRoute)
+
 app.listen(8000, (err)=>{
     if(err){
         throw err
