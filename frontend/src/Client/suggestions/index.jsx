77f8617b4bb720ce3b724a83202591ef7b3fed75
unknown <youssefhoussein701@gmail.com>
2023-12-30 19:18:09 +0200

    frontend <> handle the suggestions from the data base

--- frontend/src/Client/suggestions/index.jsx
+++ frontend/src/Client/suggestions/index.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import Navbar from '../../Components/navbar'
 import Sidebar from '../../Components/sidebar'
 import Map from '../../Components/map'
 import SuggestionCard from '../../Components/suggestionCard'
+import axios from 'axios'
 const Suggestions = () => {
   const [longitude, setLongitude] = useState(localStorage.getItem('longitude'))
   const [latitude, setLatitude] = useState(localStorage.getItem('latitude'))
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const fetchData = async () =>{
+      try{
+        const token = localStorage.getItem('token');
+        const data ={
+          userLat: latitude,
+          userLong: longitude
+        }
+        const response = await axios.post('http://127.0.0.1:8000/api/rooms/suggestions',data, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+        setSuggestions(response.data)
+      }
+      catch(err){
+        console.log("error occured")
+      }
+    }
+
+    fetchData()
+  }, [])
+  const getImageUrlFromBase64 = (base64String) => {
+    return `data:image/png;base64,${base64String}`;
+  };
   return (
     <div className='suggestions relative full'>
       <Navbar username={"Youssef"} />
@@ -15,28 +42,15 @@ const Suggestions = () => {
         <Map />
       </div>
       <div className='suggestions-cont flex column gap-10'>
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
-        <SuggestionCard />
+        {
+          suggestions.map((room,index) =>(
+            <SuggestionCard 
+              latitude={room.latitude} 
+              longitude={room.longitude} 
+              image={getImageUrlFromBase64(room.images[0].image)} 
+              features={room.features}/>
+          ))
+        }
       </div>
      
     </div>
--- frontend/src/Components/suggestionCard/index.jsx
+++ frontend/src/Components/suggestionCard/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
-const SuggestionCard = () => {
+const SuggestionCard = ({latitude,longitude,image,features}) => {
+  
   return (
     <div className='sug-card-container flex column center gap-10'>
-      <div className="room-img-cont"></div>
-      <div className="room-desc-cont"></div>
+      <div className="room-img-cont">
+        <img src={image} alt="roomImage" className='sugg-img'/>
+      </div>
+      <div className="room-desc-cont">
+        {features}
+      </div>
     </div>
   )
 }
