b8ffe8063ba396a8640dc231166c05368d372d69
unknown <youssefhoussein701@gmail.com>
2024-01-01 16:19:05 +0200

    frontend <> handle the link and design for slots

--- frontend/src/Client/slots/index.jsx
+++ frontend/src/Client/slots/index.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import Navbar from '../../Components/navbar'
 import Sidebar from '../../Components/sidebar'
 import SlotCard from '../../Components/slotCard'
+import { useEffect } from 'react'
+import axios from 'axios'
 const Slots = () => {
+  const [bookings, setBookings] = useState([])
+  useEffect(() => {
+    const fetchData = async () =>{
+      try{
+        const token = localStorage.getItem('token');
+        const data = ({
+          token,
+        })
+        const response = await axios.post('http://127.0.0.1:8000/api/booking/userBooking',data, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+        setBookings(response.data.bookings)
+      }
+      catch(err){
+        console.log("error occured")
+      }
+    }
+
+    fetchData()
+  }, [])
   return (
     <div className='slots relative full'>
       <Navbar username={"Youssef"} />
       <Sidebar items={["Home","Suggestions","Slots","Statistics","Support"]} selected={"Slots"}/>
       <div className="slots-body absolute body">
-        <SlotCard />
+      {
+          bookings.map((booking,index) =>(
+            <SlotCard 
+              date={booking.date}
+              endDate={booking.endDate}
+              latitude={booking.room.latitude}
+              longitude={booking.room.longitude}
+              images={booking.room.images}
+            />
+          ))
+        }
       </div>
     </div>
   )
--- frontend/src/Components/slotCard/index.jsx
+++ frontend/src/Components/slotCard/index.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
-const SlotCard = () => {
+import ImageSlider from '../imageSlider'
+import ModalComponent from '../modal'
+import {MapContainer, Marker, TileLayer,Popup} from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+const SlotCard = ({images,latitude, longitude,date, endDate}) => {
+  const [open, setOpen] = useState(false)
+
+  const [formattedDate, setFormattedDate] = useState('');
+  const [formattedEndDate, setFormattedEndDate] = useState('');
+
+  useEffect(() => {
+    const dateObject = new Date(date);
+    const endDateObject = new Date(endDate)
+
+    const options = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false,
+      timeZone: 'UTC',
+    };
+
+    const formattedDateUTC = dateObject.toLocaleString('en-US', options);
+    const formattedEndDateUTC = endDateObject.toLocaleString('en-US', options);
+
+    options.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    const formattedDateLocal = dateObject.toLocaleString('en-US', options);
+    const formattedEndDateLocal = endDateObject.toLocaleString('en-US', options);
+
+    setFormattedDate(formattedDateLocal)
+    setFormattedEndDate(formattedEndDateLocal)
+
+    console.log(images)
+    console.log(latitude)
+    console.log(longitude)
+    console.log(formattedDateLocal)
+    console.log(formattedEndDateLocal)
+    
+  }, [date]);
+  const openModal = () =>{
+    setOpen(true)
+  }
+  const closeModal = () =>{
+    setOpen(false)
+  }
   return (
-    <div className='slotcard-container'>
-      
+    <div className='slotcard-container flex column spaceBetween align-center'>
+      <div className="slot-image">
+        <ImageSlider images={images} />
+      </div>
+      <div className="slot-location flex center">
+        <div onClick={openModal} className='location-btn'>
+          Location
+        </div>
+      </div>
+      <div className="slot-date flex center">
+       From &nbsp; <span className='date-data'>{formattedDate}</span> 
+      </div>
+      <div className="slot-date flex center">
+      To &nbsp; <span className='date-data'>{formattedEndDate}</span> 
+      </div>
+      <ModalComponent 
+            openModal={open} 
+            onRequestClose={closeModal} 
+            posTop={'50'} 
+            posLeft={'50'} 
+            justifyContent={'flex-start'} 
+            height={'50'} gap={'0'} 
+            backgroundColor={'#081B38'} 
+            color={'white'} 
+            width={'500'}
+            direction={'column'}
+            alignItems={'center'}
+          >
+          <MapContainer className='map' center={[latitude,longitude]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[latitude,longitude]}>
+            </Marker>
+        </MapContainer>
+      </ModalComponent> 
     </div>
   )
 }
