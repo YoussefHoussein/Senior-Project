86303e841ac94c542910616e1f8378e1232c1f93
unknown <youssefhoussein701@gmail.com>
2024-01-05 20:21:48 +0200

    backend <> add the update function

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -19,147 +19,7 @@ const  generateRandomPassword = (length) => {
     return password;
   }
 
-const addRoom = async (req, res, next) =>{
-    const passwordLength = 12; 
-    const randomPassword = generateRandomPassword(passwordLength);
-    
-    const hashedPassword = await bcrypt.hash(randomPassword, 10);
 
-    const roomImagesDir = path.resolve(process.cwd(), 'room_images');
-    if (!fs.existsSync(roomImagesDir)) {
-        fs.mkdirSync(roomImagesDir);
-        console.log('Created "room_images" directory');
-    }
-    const {description, latitude, longitude, base64Images, userType, country} = req.body;
-    if(userType === 2){
-        res.json({
-            message: "You can not handle this process"
-        });
-        return;
-    }
-    try{
-        if (latitude === 0 && longitude === 0) {
-            res.json({
-                message: "missing data - latitude and longitude are both zero"
-            });
-            return;
-        }
-        if (latitude == null || isNaN(latitude) || longitude == null || isNaN(longitude)) {
-            res.json({
-                message: "missing data - latitude and/or longitude are null, undefined, or NaN"
-            });
-            return;
-        }
-        if(!description || !base64Images){
-            res.json({
-                message: "missing data",
-                description: description,
-                latitude: latitude,
-                longitude: longitude,
-                base64Images: base64Images
-            })
-            return
-        }
-        const existingRoom = await Room.findOne({ latitude, longitude });
-
-        if (existingRoom) {
-            res.json({
-                message: "room already exists"
-            })
-            return
-        }
-
-        const roomFolder = path.join(roomImagesDir, `room_${uuidv4()}`);
-        fs.mkdirSync(roomFolder);
-
-        const savedImageObjects = base64Images.map((base64Image, index) => {
-            const imageBuffer = Buffer.from(base64Image.split(',')[1], 'base64');
-            const imageName = `image_${index + 1}.png`;
-            const imagePath = path.join(roomFolder, imageName);
-        
-            fs.writeFileSync(imagePath, imageBuffer);
-        
-            return { image: imageName, folderName: roomFolder }; 
-        });
-
-          const roomData = new Room({
-            features: description,
-            images: savedImageObjects,
-            latitude,
-            longitude,
-            password: hashedPassword,
-            country: country
-          })
-      
-        const savedRoom = await roomData.save();
-        res.json({
-            message: "room saved successfully",
-            room: savedRoom,
-        })
-        return
-    }
-    catch (err) {
-        console.error('Error saving room:', err);
-        res.status(500).json({
-            message: 'Error saving room',
-            error: err,
-        });
-}
-}
-const updateRoom = async (req, res, next) => {
-    const { roomId, description, latitude, longitude, base64Images, country } = req.body;
-
-    try {
-        const roomToUpdate = await Room.findById(roomId);
-
-        if (!roomToUpdate) {
-            res.status(404).json({
-                message: "Room not found",
-            });
-            return;
-        }
-
-        roomToUpdate.features = description;
-        roomToUpdate.latitude = latitude;
-        roomToUpdate.longitude = longitude;
-        roomToUpdate.country = country;
-
-
-        if (base64Images && base64Images.length > 0) {
-
-            roomToUpdate.images.forEach((image) => {
-                const imagePath = path.join(image.folderName, image.image);
-                fs.unlinkSync(imagePath);
-            });
-
-
-            const savedImageObjects = base64Images.map((base64Image, index) => {
-                const imageBuffer = Buffer.from(base64Image.split(',')[1], 'base64');
-                const imageName = `image_${index + 1}.png`;
-                const imagePath = path.join(roomToUpdate.images[0].folderName, imageName);
-
-                fs.writeFileSync(imagePath, imageBuffer);
-
-                return { image: imageName, folderName: roomToUpdate.images[0].folderName };
-            });
-
-            roomToUpdate.images = savedImageObjects;
-        }
-
-        const updatedRoom = await roomToUpdate.save();
-
-        res.json({
-            message: "Room updated successfully",
-            room: updatedRoom,
-        });
-    } catch (err) {
-        console.error('Error updating room:', err);
-        res.status(500).json({
-            message: 'Error updating room',
-            error: err,
-        });
-    }
-};
 
 const getRoomImagesAsBase64 = async (room) => {
     const imagePromises = room.images.map(async (image) => {
@@ -274,7 +134,93 @@ const search = async (req, res) => {
         });
     }
 }
+const addRoom = async (req, res, next) =>{
+    const passwordLength = 12; 
+    const randomPassword = generateRandomPassword(passwordLength);
+    
+    const hashedPassword = await bcrypt.hash(randomPassword, 10);
 
+    const roomImagesDir = path.resolve(process.cwd(), 'room_images');
+    if (!fs.existsSync(roomImagesDir)) {
+        fs.mkdirSync(roomImagesDir);
+        console.log('Created "room_images" directory');
+    }
+    const {description, latitude, longitude, base64Images, userType, country} = req.body;
+    if(userType === 2){
+        res.json({
+            message: "You can not handle this process"
+        });
+        return;
+    }
+    try{
+        if (latitude === 0 && longitude === 0) {
+            res.json({
+                message: "missing data - latitude and longitude are both zero"
+            });
+            return;
+        }
+        if (latitude == null || isNaN(latitude) || longitude == null || isNaN(longitude)) {
+            res.json({
+                message: "missing data - latitude and/or longitude are null, undefined, or NaN"
+            });
+            return;
+        }
+        if(!description || !base64Images){
+            res.json({
+                message: "missing data",
+                description: description,
+                latitude: latitude,
+                longitude: longitude,
+                base64Images: base64Images
+            })
+            return
+        }
+        const existingRoom = await Room.findOne({ latitude, longitude });
+
+        if (existingRoom) {
+            res.json({
+                message: "room already exists"
+            })
+            return
+        }
+
+        const roomFolder = path.join(roomImagesDir, `room_${uuidv4()}`);
+        fs.mkdirSync(roomFolder);
+
+        const savedImageObjects = base64Images.map((base64Image, index) => {
+            const imageBuffer = Buffer.from(base64Image.split(',')[1], 'base64');
+            const imageName = `image_${index + 1}.png`;
+            const imagePath = path.join(roomFolder, imageName);
+        
+            fs.writeFileSync(imagePath, imageBuffer);
+        
+            return { image: imageName, folderName: roomFolder }; 
+        });
+
+          const roomData = new Room({
+            features: description,
+            images: savedImageObjects,
+            latitude,
+            longitude,
+            password: hashedPassword,
+            country: country
+          })
+      
+        const savedRoom = await roomData.save();
+        res.json({
+            message: "room saved successfully",
+            room: savedRoom,
+        })
+        return
+    }
+    catch (err) {
+        console.error('Error saving room:', err);
+        res.status(500).json({
+            message: 'Error saving room',
+            error: err,
+        });
+}
+}
 const deleteRoom = async (req, res, next) => {
     const { room_id } = req.body;
     try {
@@ -312,5 +258,68 @@ const deleteRoom = async (req, res, next) => {
         });
     }
 };
+
+
+const updateRoom = async (req, res, next) => {
+    const { room_id, images, latitude, longitude, description, country } = req.body;
+
+    try {
+        const currentDate = new Date();
+        const bookings = await Booking.find({ room: room_id, endDate: { $gte: currentDate } });
+        if (bookings && bookings.length > 0) {
+            res.json({
+                message: "Room has active bookings, you can't delete it."
+            });
+            return;
+        }
+
+        const room = await Room.findById(room_id);
+        if (!room) {
+            res.json({
+                message: "Room not found."
+            });
+            return;
+        }
+
+        const roomFolder = path.resolve(process.cwd(), 'room_images', room.images[0].folderName);
+        await fse.remove(roomFolder);
+
+        await Room.deleteOne({ _id: room_id });
+    } catch (err) {
+        console.error('Error deleting room:', err);
+        res.status(500).json({
+            message: 'Error occurred',
+            error: err,
+        });
+        return;
+    }
+
+    try {
+
+        await addRoom({
+            body: {
+                description,
+                latitude,
+                longitude,
+                base64Images: images,
+                userType: 1,
+                country,
+            }
+        }, res, next);
+
+        res.json({
+            message: 'Room updated successfully.',
+        });
+    } catch (error) {
+        console.error('Error updating room:', error);
+        res.status(500).json({
+            message: 'Error occurred during room update',
+            error,
+        });
+    }
+};
+
+
+
 
 module.exports = {addRoom, suggestions,getRoomImagesAsBase64,deleteRoom, search, updateRoom}
